b7c690974cd142ff3983302e2f7d644da6ab725a
pranavanba <pranavanba@gmail.com>
2023-01-23 15:25:08 -0800

    Created function that works on any concept for specific file export type (e.g., DailyData)

--- main.R
+++ main.R
@@ -33,16 +33,33 @@ concept_map <- as_tibble(read.csv('i2b2conceptmap.csv', skip = 1))
 
 
 # Convert to i2b2 concept -------------------------------------------------
-# Specific examples with a few concepts
 
-# Body BMI
-bodybmi_path <- concept_map$concept_cd[1]
+# Specific example using any DailyData concept
+dd_path <- concept_map$concept_cd[4]
 
-fitbit_bodybmi <- DailyData_csv %>% 
-  select(ParticipantIdentifier, Date, BodyBmi) %>% 
-  mutate(BodyBmi = as.numeric(BodyBmi)) %>% 
-  mutate(unit = concept_map$UNITS_CD[which(concept_map$concept_cd==bodybmi_path)]) %>% 
-  mutate(valtype = typeof(BodyBmi)) %>% 
-  mutate(definition = concept_map$Definition[which(concept_map$concept_cd==bodybmi_path)])
+fitbit_dd_example <- DailyData_csv %>% 
+  select(ParticipantIdentifier, Date, BodyWeight) %>% 
+  # mutate(BodyBmi = as.numeric(BodyFat)) %>% 
+  mutate(unit = concept_map$UNITS_CD[which(concept_map$concept_cd==dd_path)]) %>% 
+  mutate(valtype = typeof(BodyWeight)) %>% 
+  mutate(definition = concept_map$Definition[which(concept_map$concept_cd==dd_path)])
 
+# Function to use for all DailyData concepts
+fx <- function(concept, map, data) {
+  fact_path <- map %>% 
+    select(concept_cd) %>% 
+    filter(grepl(paste0("Fitbit:DailyData:",concept,"$"), concept_cd, ignore.case = T))
+  
+  concept <- data %>% 
+    select(ParticipantIdentifier, Date, concept) %>% 
+    # mutate(unit = map$UNITS_CD[which(map$concept_cd==fact_path)]) %>%
+    mutate(valtype = typeof(.[[3]]))
+    # mutate(definition = map$Definition[which(map$concept_cd==fact_path)])
+  
+  # return(concept)
+}
+
+BodyBmi <- fx("BodyBmi", concept_map, DailyData_csv)
+Calories <- fx("Calories", concept_map, DailyData_csv)
+BodyWeight <- fx("BodyWeight", concept_map, DailyData_csv)
 
