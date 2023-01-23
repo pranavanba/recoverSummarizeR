cead9b7f993de775ae8a16ea6d8a006e7bd53d96
pranavanba <pranavanba@gmail.com>
2023-01-23 14:12:04 -0800

    Process i2b2 concept from file example

--- main.R
+++ main.R
@@ -23,10 +23,26 @@ file.id %>%
 
 # Parse file(s) to dataframe -------------------------------------------------
 
-tmpjson <- stream_in(con = file('data/FitbitActivityLogs_20220111-20230103.json'))
-tmpcsv <- as_tibble(read.csv('data/FitbitActivityLogs_20221101-20230103.csv'))
+DailyData_json <- stream_in(con = file('data/FitbitDailyData_20220111-20230103.json'))
+DailyData_csv <- as_tibble(read.csv('data/FitbitDailyData_20221101-20230103.csv'))
 
 
 # Process i2b2 concept map ------------------------------------------------
 
 concept_map <- as_tibble(read.csv('i2b2conceptmap.csv', skip = 1))
+
+
+# Convert to i2b2 concept -------------------------------------------------
+# Specific examples with a few concepts
+
+# Body BMI
+bodybmi_path <- concept_map$concept_cd[1]
+
+fitbit_bodybmi <- DailyData_csv %>% 
+  select(ParticipantIdentifier, Date, BodyBmi) %>% 
+  mutate(BodyBmi = as.numeric(BodyBmi)) %>% 
+  mutate(unit = concept_map$UNITS_CD[which(concept_map$concept_cd==bodybmi_path)]) %>% 
+  mutate(valtype = typeof(BodyBmi)) %>% 
+  mutate(definition = concept_map$Definition[which(concept_map$concept_cd==bodybmi_path)])
+
+
