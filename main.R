4e2a57f54e3b74918d62be026fcfbdcc97223357
pranavanba <pranavanba@gmail.com>
2023-01-23 12:22:57 -0800

    Import libraries

---
+++ main.R
@@ -0,0 +1,10 @@
+# Import Libraries --------------------------------------------------------
+
+# install.packages("install.load")
+library(install.load)
+install_load("dplyr","tidyr", "magrittr", "tibble", "devtools")
+# install_github(
+#   repo="https://github.com/generalui/synapser/tree/reticulate",
+#   ref="reticulate")
+library(synapser)
+
