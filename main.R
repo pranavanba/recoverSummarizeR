8fccdf334380a14b289aa2169c8593abdb71d4d9
pranavanba <pranavanba@gmail.com>
2023-01-23 13:32:38 -0800

    Parse json/csv files to dataframe

--- main.R
+++ main.R
@@ -2,7 +2,7 @@
 
 # install.packages("install.load")
 library(install.load)
-install_load("dplyr","tidyr", "magrittr", "tibble", "devtools")
+install_load("dplyr","tidyr", "magrittr", "tibble", "devtools", "jsonlite")
 # install_github(
 #   repo="https://github.com/generalui/synapser/tree/reticulate",
 #   ref="reticulate")
@@ -13,9 +13,17 @@ library(synapser)
 
 synLogin()
 
-file.id <- "syn50894469"
+file.id <- "syn50894470"
 
 file.id %>% 
   synGet() %>% 
   {.$path} %>% 
   unzip(exdir = 'data')
+
+
+# Parse file(s) to dataframe -------------------------------------------------
+
+tmpjson <- stream_in(con = file('data/FitbitActivityLogs_20220111-20230103.json'))
+tmpcsv <- as_tibble(read.csv('data/FitbitActivityLogs_20221101-20230103.csv'))
+
+
