813024b4e9b02a14a8d2dd6e2e2dbed998e87042
pranavanba <pranavanba@gmail.com>
2023-01-24 13:20:51 -0800

    Minor edits to code readability

--- main.R
+++ main.R
@@ -13,7 +13,7 @@ library(synapser)
 
 synLogin()
 
-file.id <- "syn50894469"
+file.id <- "syn50894470"
 
 file.id %>% 
   synGet() %>% 
@@ -22,9 +22,9 @@ file.id %>%
 
 
 # Parse file(s) to dataframe -------------------------------------------------
-
-DailyData_json <- stream_in(con = file('raw-data/FitbitDailyData_20220111-20230103.json'))
-# DailyData_csv <- as_tibble(read.csv('raw-data/FitbitDailyData_20221101-20230103.csv'))
+# File format depends on what is available in the data export from Care Evolution (e.g. it can be CSV and/or JSON)
+# DailyData_json <- stream_in(con = file('raw-data/FitbitDailyData_20220111-20230103.json'))
+DailyData_csv <- as_tibble(read.csv('raw-data/FitbitDailyData_20221101-20230103.csv'))
 
 
 # Process i2b2 concept map ------------------------------------------------
@@ -32,34 +32,40 @@ DailyData_json <- stream_in(con = file('raw-data/FitbitDailyData_20220111-202301
 concept_map <- as_tibble(read.csv('i2b2conceptmap.csv', skip = 1))
 
 
-# Convert to i2b2 concept -------------------------------------------------
+# Convert to i2b2 concept format -------------------------------------------------
+## Specific example using one concept =====================================
+# Can use any DailyData concept
+path <- concept_map$concept_cd[4]
 
-# Specific example using any DailyData concept
-dd_path <- concept_map$concept_cd[4]
-
-fitbit_dd_example <- DailyData_csv %>% 
+example_df <- DailyData_csv %>% 
   select(ParticipantIdentifier, Date, BodyWeight) %>% 
-  # mutate(BodyBmi = as.numeric(BodyFat)) %>% 
-  mutate(unit = concept_map$UNITS_CD[which(concept_map$concept_cd==dd_path)]) %>% 
+  mutate(unit = concept_map$UNITS_CD[which(concept_map$concept_cd==path)]) %>% 
   mutate(valtype = typeof(BodyWeight)) %>% 
-  mutate(definition = concept_map$Definition[which(concept_map$concept_cd==dd_path)])
+  mutate(definition = concept_map$Definition[which(concept_map$concept_cd==path)])
 
-# Function to use for all DailyData concepts
-fx <- function(concept, map, data) {
-  fact_path <- map %>% 
+
+## Function for all concepts ==============================================
+# Can use for all DailyData concepts
+fx <- function(concept, export, map, data) {
+  path <- map %>% 
     select(concept_cd) %>% 
-    filter(grepl(paste0("Fitbit:DailyData:",concept,"$"), concept_cd, ignore.case = T))
+    filter(grepl(paste0("Fitbit:",export,":",concept,"$"), concept_cd, ignore.case = T)) %>% 
+    as.character()
   
   concept <- data %>% 
     select(ParticipantIdentifier, Date, concept) %>% 
-    # mutate(unit = map$UNITS_CD[which(map$concept_cd==fact_path)]) %>%
+    # mutate(unit = map$UNITS_CD[which(map$concept_cd==path)]) %>%
     mutate(valtype = typeof(.[[3]]))
-    # mutate(definition = map$Definition[which(map$concept_cd==fact_path)])
+    # mutate(definition = map$Definition[which(map$concept_cd==path)])
   
   # return(concept)
 }
 
-BodyBmi <- fx("BodyBmi", concept_map, DailyData_csv)
-Calories <- fx("Calories", concept_map, DailyData_csv)
-BodyWeight <- fx("BodyWeight", concept_map, DailyData_csv)
+
+# Test the function -------------------------------------------------------
+
+
+BodyBmi <- fx("BodyBmi", "DailyData", concept_map, DailyData_csv)
+Calories <- fx("Calories", "DailyData", concept_map, DailyData_csv)
+BodyWeight <- fx("BodyWeight", "DailyData", concept_map, DailyData_csv)
 
