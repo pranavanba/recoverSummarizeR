78f791ff8d8643095ea6a3dbb7b13a0bbfcf3a10
pranavanba <pranavanba@gmail.com>
2023-03-09 10:51:33 -0800

    Minor edits; commented old code

--- main.R
+++ main.R
@@ -27,8 +27,8 @@ synLogin()
 
 dir.create("raw-data")
 setwd("raw-data/")
-dir.create("parquet-pilot-datasets")
-setwd("parquet-pilot-datasets/")
+dir.create("parquet-datasets")
+setwd("parquet-datasets/")
 system("synapse get -r syn50996868")
 setwd("..")
 setwd("..")
@@ -41,10 +41,11 @@ concept_map <-
     "https://docs.google.com/spreadsheets/d/1XagFptBLxk5UW5CzZl-2gA8ncqwWk6XcGVFFSna2R_s/edit?usp=share_link"
   )
 
+break
 
 # Read parquet files to df ------------------------------------------------
 
-parent_directory <- "raw-data/parquet-pilot-datasets"
+parent_directory <- "raw-data/parquet-datasets"
 
 file_paths <-
   list.files(path = parent_directory,
@@ -68,123 +69,124 @@ names(tmp) <-
 
 # Exclude manifest and HK ecg parent files for now
 tmp <- tmp[!grepl("MANIFEST", names(tmp))]
-tmp <-
-  tmp[!grepl("healthkitv2electrocardiogram - HealthKitV2Electrocardiogram",
-             names(tmp))]
+tmp <- tmp[!grepl("healthkitv2electrocardiogram - HealthKitV2Electrocardiogram", names(tmp))]
 names(tmp) <- sub("-.*\\.snappy", "", names(tmp))
 names(tmp) <- sub("dataset_", "", names(tmp))
+names(tmp) <- gsub(" ", "", names(tmp))
 
 # May need to update the following to accommodate multiple multi-part parquet
 # files; currently works with only one mutli-part file: fitbitintradaycombined
 # was the only multi-part parquet file found. Perhaps combine the multi-part
 # files inside the respective raw-data nested folder before reading all into a
 # df.
-multi_part_dfs <-
-  list(names(tmp)[duplicated(names(tmp))] %>% unique())
+multi_part_dfs <- list(names(tmp)[duplicated(names(tmp))] %>% unique())
+
 tmp_df_list <- bind_rows(tmp[which(names(tmp) == multi_part_dfs)])
+
 tmp2 <- tmp[!grepl(multi_part_dfs, names(tmp))]
+
 df_list <- c(tmp2, list(tmp_df_list))
+
 names(df_list)[which(!(unique(names(df_list)) %in% unique(names(tmp))))] <-
   unique(names(tmp))[which(!(unique(names(tmp)) %in% unique(names(df_list))))]
+
 rm(tmp, tmp2, tmp_df_list, multi_part_dfs)
 
 
 # Convert to i2b2 concept format -------------------------------------------------
 
-## Specific example using one concept
-
-# Can use any DailyData concept
-path <- concept_map$concept_cd[4]
-
-example_df <- DailyData_csv %>%
-  select(ParticipantIdentifier, Date, BodyWeight) %>%
-  mutate(unit = concept_map$UNITS_CD[which(concept_map$concept_cd == path)]) %>%
-  mutate(valtype = typeof(BodyWeight)) %>%
-  mutate(definition = concept_map$Definition[which(concept_map$concept_cd ==
-                                                     path)])
+# ## Specific example using one concept
+# 
+# # Can use any DailyData concept
+# path <- concept_map$concept_cd[4]
+# 
+# example_df <- DailyData_csv %>%
+#   select(ParticipantIdentifier, Date, BodyWeight) %>%
+#   mutate(unit = concept_map$UNITS_CD[which(concept_map$concept_cd == path)]) %>%
+#   mutate(valtype = typeof(BodyWeight)) %>%
+#   mutate(definition = concept_map$Definition[which(concept_map$concept_cd==path)])
 
 
 ## Function for all concepts ==============================================
 
-# Can use for all DailyData concepts
-
-# 1) Filter the 'map' data frame to include rows only where 'concept' ends with
-# 'export:concept'
-# 2) Create the final data frame by binding the relevant
-# columns from 'data' data frame with those from the filtered map
-
-fx <- function(concept,
-               export,
-               path_to_map_csv,
-               path_to_data_csv) {
-  data <- as_tibble(read.csv(data_path))
-  
-  map <- as_tibble(read.csv(map_csv, skip = 1))
-  
-  map_filtered <- map %>%
-    filter(str_ends(concept_cd, paste0(export, ":", concept))) %>%
-    select(concept_cd, UNITS_CD, Definition)
-  
-  out <- data %>%
-    select(ParticipantIdentifier, Date, concept) %>%
-    mutate(value_type = typeof(.[[3]])) %>%
-    bind_cols(select(map_filtered, UNITS_CD, Definition))
-  
-  # return(out)
-}
-
-
-# Test the function
-
-BodyBmi <-
-  fx(
-    "BodyBmi",
-    "DailyData",
-    "i2b2conceptmap.csv",
-    "raw-data/FitbitDailyData_20221101-20230103.csv"
-  )
-Calories <-
-  fx(
-    "Calories",
-    "DailyData",
-    "i2b2conceptmap.csv",
-    "raw-data/FitbitDailyData_20221101-20230103.csv"
-  )
-BodyWeight <-
-  fx(
-    "BodyWeight",
-    "DailyData",
-    "i2b2conceptmap.csv",
-    "raw-data/FitbitDailyData_20221101-20230103.csv"
-  )
+# # Can use for all DailyData concepts
+# 
+# # 1) Filter the 'map' data frame to include rows only where 'concept' ends with
+# # 'export:concept'
+# # 2) Create the final data frame by binding the relevant
+# # columns from 'data' data frame with those from the filtered map
+# 
+# fx <- function(concept,
+#                export,
+#                path_to_map_csv,
+#                path_to_data_csv) {
+#   data <- as_tibble(read.csv(data_path))
+#   
+#   map <- as_tibble(read.csv(map_csv, skip = 1))
+#   
+#   map_filtered <- map %>%
+#     filter(str_ends(concept_cd, paste0(export, ":", concept))) %>%
+#     select(concept_cd, UNITS_CD, Definition)
+#   
+#   out <- data %>%
+#     select(ParticipantIdentifier, Date, concept) %>%
+#     mutate(value_type = typeof(.[[3]])) %>%
+#     bind_cols(select(map_filtered, UNITS_CD, Definition))
+#   
+#   # return(out)
+# }
+# 
+# 
+# # Test the function
+# 
+# BodyBmi <-
+#   fx(
+#     "BodyBmi",
+#     "DailyData",
+#     "i2b2conceptmap.csv",
+#     "raw-data/FitbitDailyData_20221101-20230103.csv"
+#   )
+# Calories <-
+#   fx(
+#     "Calories",
+#     "DailyData",
+#     "i2b2conceptmap.csv",
+#     "raw-data/FitbitDailyData_20221101-20230103.csv"
+#   )
+# BodyWeight <-
+#   fx(
+#     "BodyWeight",
+#     "DailyData",
+#     "i2b2conceptmap.csv",
+#     "raw-data/FitbitDailyData_20221101-20230103.csv"
+#   )
 
 
 # Mock example for single df output ---------------------------------------
 
-mock <- bind_rows(BodyBmi, BodyWeight, Calories)
-mock %<>% rename(Value = Variable)
-mock %<>% mutate(Variable = "")
-mock$Variable[which(grepl("Index", mock$Definition))] <- "BodyBmi"
-mock$Variable[which(grepl("weight", mock$Definition))] <-
-  "BodyWeight"
-mock$Variable[which(grepl("exercise", mock$Definition))] <-
-  "Calories"
-mock %<>% select(ParticipantIdentifier,
-                 Date,
-                 Variable,
-                 Value,
-                 value_type,
-                 UNITS_CD,
-                 Definition)
-bind_rows(mock[1:5,], mock[483:487,], mock[965:969,]) %>% View()
+# mock <- bind_rows(BodyBmi, BodyWeight, Calories)
+# mock %<>% rename(Value = Variable)
+# mock %<>% mutate(Variable = "")
+# mock$Variable[which(grepl("Index", mock$Definition))] <- "BodyBmi"
+# mock$Variable[which(grepl("weight", mock$Definition))] <-
+#   "BodyWeight"
+# mock$Variable[which(grepl("exercise", mock$Definition))] <-
+#   "Calories"
+# mock %<>% select(ParticipantIdentifier,
+#                  Date,
+#                  Variable,
+#                  Value,
+#                  value_type,
+#                  UNITS_CD,
+#                  Definition)
+# bind_rows(mock[1:5,], mock[483:487,], mock[965:969,]) %>% View()
 
 
 # Find common columns to merge on ---------------------------------------------------------------------------------
 
 # Common cols between fitbit sleep log files
-identical((df_list$`fitbitsleeplogs ` %>% count(LogId) %>% .[1] %>% distinct()),
-          (df_list$`fitbitsleeplogs_sleeplogdetails ` %>% count(LogId) %>% .[1] %>% distinct())
-)
+identical((df_list$fitbitsleeplogs %>% count(LogId) %>% .[1] %>% distinct()),
+          (df_list$fitbitsleeplogs_sleeplogdetails %>% count(LogId) %>% .[1] %>% distinct()))
 
 
 # Data Summarization ----------------------------------------------------------------------------------------------
@@ -235,6 +237,7 @@ new_factors <- lapply(new_df_list, function(x) x[["measurement"]] %>% unique())
 # For summaries: create fx for each type of summary --> all time vs weekly, add case switching within both depending on 5/95 percentile, mean, med, var, count
 
 summary <- function(timescale, type) {
+  
   alltime <- function(type) {
     switch(type,
       pct5 = new_df_list[["fitbitactivitylogs "]] %>%
