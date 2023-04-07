6aa1155c05484bc28dee6dae1347ba05e2c39bad
pranavanba <pranavanba@gmail.com>
2023-04-07 14:22:25 -0700

    Initial commit; separated from main.R

---
+++ get_data.R
@@ -0,0 +1,62 @@
+# Import Libraries --------------------------------------------------------
+
+# install.packages("install.load")
+library(install.load)
+install_load(
+  "dplyr",
+  "tidyr",
+  "magrittr",
+  "tibble",
+  "devtools",
+  "jsonlite",
+  "stringr",
+  "arrow",
+  "googlesheets4",
+  "readr",
+  "reshape2",
+  "lubridate"
+)
+library(synapser)
+
+
+# Get data ----------------------------------------------------------------
+
+synLogin()
+
+if (dir.exists("raw-data")) {
+  setwd("raw-data/")
+  if (dir.exists("parquet-datasets")) {
+    setwd("parquet-datasets/")
+    system("synapse get -r syn50996868")
+    setwd("..")
+    setwd("..")
+  } else {
+    dir.create("parquet-datasets")
+    setwd("parquet-datasets/")
+    system("synapse get -r syn50996868")
+    setwd("..")
+    setwd("..")
+  }
+} else {
+  dir.create("raw-data")
+  setwd("raw-data/")
+  dir.create("parquet-datasets")
+  setwd("parquet-datasets/")
+  system("synapse get -r syn50996868")
+  setwd("..")
+  setwd("..")
+}
+
+# Get i2b2 concepts map ---------------------------------------------------
+
+get_concepts <- function(url, token_refresh) {
+  x <- googlesheets4::read_sheet(url)
+  token_refresh
+  x$concept_cd %<>% tolower()
+  return(x)
+}
+
+concepts_url <- "https://docs.google.com/spreadsheets/d/1XagFptBLxk5UW5CzZl-2gA8ncqwWk6XcGVFFSna2R_s/edit?usp=share_link"
+concept_map <- get_concepts(concepts_url, token_refresh = 1)
+rm(concepts_url)
+
