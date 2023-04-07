1cfc17ab36c41584cdf777eda3161feeb877c0f0
pranavanba <pranavanba@gmail.com>
2023-04-07 14:22:52 -0700

    Moved code for fetching data to separate file

--- main.R
+++ main.R
@@ -19,47 +19,6 @@ install_load(
 library(synapser)
 
 
-# Get data ----------------------------------------------------------------
-
-synLogin()
-
-if (dir.exists("raw-data")) {
-  setwd("raw-data/")
-  if (dir.exists("parquet-datasets")) {
-    setwd("parquet-datasets/")
-    system("synapse get -r syn50996868")
-    setwd("..")
-    setwd("..")
-  } else {
-    dir.create("parquet-datasets")
-    setwd("parquet-datasets/")
-    system("synapse get -r syn50996868")
-    setwd("..")
-    setwd("..")
-  }
-} else {
-  dir.create("raw-data")
-  setwd("raw-data/")
-  dir.create("parquet-datasets")
-  setwd("parquet-datasets/")
-  system("synapse get -r syn50996868")
-  setwd("..")
-  setwd("..")
-}
-
-# Get i2b2 concepts map ---------------------------------------------------
-
-get_concepts <- function(url, token_refresh) {
-  x <- googlesheets4::read_sheet(url)
-  token_refresh
-  x$concept_cd %<>% tolower()
-  return(x)
-}
-
-concepts_url <- "https://docs.google.com/spreadsheets/d/1XagFptBLxk5UW5CzZl-2gA8ncqwWk6XcGVFFSna2R_s/edit?usp=share_link"
-concept_map <- get_concepts(concepts_url, token_refresh = 1)
-rm(concepts_url)
-
 # Read parquet files to df ------------------------------------------------
 
 parent_directory <- "raw-data/parquet-datasets"
