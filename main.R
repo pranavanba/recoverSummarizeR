516aa962a516ed4535e33ced6fa32d725e499302
pranavanba <pranavanba@gmail.com>
2023-04-26 15:44:12 -0700

    Updated store_in_syn() executed_param to use URL for Sage-Bio forked repo

--- main.R
+++ main.R
@@ -347,7 +347,7 @@ store_in_syn <- function(synFolderID, filepath, used_param = NULL, executed_para
 }
 
 synFolderID <- "syn51184127"
-store_in_syn(synFolderID, 'output_concepts.csv', used_param = ontologyFileID, executed_param = "https://github.com/pranavanba/recoverSummarizeR/blob/release/main.R")
+store_in_syn(synFolderID, 'output_concepts.csv', used_param = ontologyFileID, executed_param = "https://github.com/Sage-Bionetworks/recoverSummarizeR/blob/main/main.R")
 store_in_syn(synFolderID, 'concepts_map.csv', used_param = ontologyFileID)
 rm(synFolderID, ontologyFileID)
 
