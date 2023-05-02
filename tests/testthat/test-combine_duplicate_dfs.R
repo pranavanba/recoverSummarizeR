be0b601138a6b22d4600e60d50669407a6cad375
pranavanba <pranavanba@gmail.com>
2023-05-02 14:11:05 -0700

    Initial commit

---
+++ tests/testthat/test-combine_duplicate_dfs.R
@@ -0,0 +1,15 @@
+test_that("combining data frames works", {
+  df1 <- data.frame(x = 1:3, y = letters[1:3])
+  df2 <- data.frame(x = 4:6, y = letters[4:6])
+  df3 <- data.frame(x = 7:9, y = letters[7:9])
+  df4 <- data.frame(x = 10:12, y = letters[10:12])
+  df_list <- list(df1, df2, df1, df3, df4, df2)
+  names(df_list) <- c("df1", "df2", "df1", "df3", "df4", "df2")
+  combined_df_list <- combine_duplicate_dfs(df_list)
+  
+  expect_equal(combined_df_list$df1, df1 %>% dplyr::bind_rows(df1))
+  expect_equal(combined_df_list$df2, df2 %>% dplyr::bind_rows(df2))
+  expect_equal(combined_df_list$df3, df3)
+  expect_equal(combined_df_list$df4, df4)
+  expect_length(combined_df_list, 4)
+})
