a5b4d22ab35dd267dfa777d158b7a3a1baadf242
pranavanba <pranavanba@gmail.com>
2023-05-05 14:07:07 -0700

    Renamed mainflow to summarize_pipeline

--- R/summarize_pipeline.R
+++ R/summarize_pipeline.R
@@ -1,4 +1,4 @@
-mainflow <- function(ontologyFileID, parquetDirID, dataset_name_filter, concept_replacements, concept_filter_col, synFolderID) {
+summarize_pipeline <- function(ontologyFileID, parquetDirID, dataset_name_filter, concept_replacements, concept_filter_col, synFolderID) {
   concept_map <- get_concept_map(ontologyFileID)
   
   df_list_original <- synget_parquet_to_df(parquetDirID, dataset_name_filter)
@@ -8,6 +8,7 @@ mainflow <- function(ontologyFileID, parquetDirID, dataset_name_filter, concept_
     lapply(function(x) {
       names(x) <- tolower(names(x))
       return(x)})
+  
   df_list <- 
     df_list_unified_tmp %>% 
     lapply(function(df) {
@@ -16,6 +17,7 @@ mainflow <- function(ontologyFileID, parquetDirID, dataset_name_filter, concept_
     })
   
   concept_replacements_reversed <- reverse_str_pairs(concept_replacements)
+  
   excluded_concepts <- diff_concepts(df_list, concept_replacements, concept_map, concept_filter_col)
   
   df_list_melted_filtered <- 
@@ -23,33 +25,32 @@ mainflow <- function(ontologyFileID, parquetDirID, dataset_name_filter, concept_
     lapply(melt_df, excluded_concepts) %>% 
     lapply(function(x) {
       x %>% 
-        select(if("participantidentifier" %in% colnames(x)) "participantidentifier",
-               matches("(?<!_)date(?!_)", perl = T),
-               if("concept" %in% colnames(x)) "concept",
-               if("value" %in% colnames(x)) "value")
-    }) %>% 
+        dplyr::select(if("participantidentifier" %in% colnames(x)) "participantidentifier",
+                      dplyr::matches("(?<!_)date(?!_)", perl = T),
+                      if("concept" %in% colnames(x)) "concept",
+                      if("value" %in% colnames(x)) "value")}) %>% 
     {Filter(function(df) "concept" %in% colnames(df), .)} %>% 
-    lapply(drop_na, "value")
+    lapply(tidyr::drop_na, "value")
   
-  df_list_melted_filtered %<>% convert_col_to_numeric()
+  df_list_melted_filtered <- df_list_melted_filtered %>% convert_col_to_numeric()
   
   df_summarized <- 
     df_list_melted_filtered %>% 
     {Filter(function(df) "concept" %in% colnames(df), .)} %>% 
     lapply(stat_summarize) %>% 
-    bind_rows() %>% 
-    distinct()
+    dplyr::bind_rows() %>% 
+    dplyr::distinct()
   
   output_concepts <- 
     process_df(df_summarized, concept_map, concept_replacements_reversed) %>% 
-    mutate(nval_num = signif(nval_num, 9)) %>% 
-    arrange(concept) %>% 
-    mutate(across(.fns = as.character)) %>% 
+    dplyr::mutate(nval_num = signif(nval_num, 9)) %>% 
+    dplyr::arrange(concept) %>% 
+    dplyr::mutate(dplyr::across(.cols = dplyr::everything(), .fns = as.character)) %>% 
     replace(is.na(.), "<null>") %>% 
-    filter(nval_num != "<null>" | tval_char != "<null>")
+    dplyr::filter(nval_num != "<null>" | tval_char != "<null>")
   
-  write.csv(output_concepts, file = 'output_concepts.csv', row.names = F)
-  write.csv(concept_map, file = 'concepts_map.csv', row.names = F)
+  utils::write.csv(output_concepts, file = 'output_concepts.csv', row.names = F)
+  utils::write.csv(concept_map, file = 'concepts_map.csv', row.names = F)
   
   store_in_syn(synFolderID, 'output_concepts.csv', used_param = ontologyFileID, executed_param = "https://github.com/Sage-Bionetworks/recoverSummarizeR/tree/release")
   store_in_syn(synFolderID, 'concepts_map.csv', used_param = ontologyFileID)
--- tests/testthat/test-summarize_pipeline.R
+++ tests/testthat/test-summarize_pipeline.R
@@ -1,4 +1,6 @@
 test_that("function works correctly", {
+  synapser::synLogin()
+
   ontologyFileID <- "syn51320791"
   parquetDirID <- "syn50996868"
   dataset_name_filter <- "fitbit"
@@ -10,26 +12,33 @@ test_that("function works correctly", {
                             "sleepbrth" = "sleepsummarybreath")
   concept_filter_col <- "concept_cd"
   synFolderID <- "syn51184127"
-  mainflow(ontologyFileID = ontologyFileID, 
-           parquetDirID = parquetDirID, 
-           concept_replacements = concept_replacements, 
-           concept_filter_col = concept_filter_col, 
+
+  summarize_pipeline(ontologyFileID = ontologyFileID,
+           parquetDirID = parquetDirID,
+           dataset_name_filter = dataset_name_filter,
+           concept_replacements = concept_replacements,
+           concept_filter_col = concept_filter_col,
            synFolderID = synFolderID)
+  
+  newest_output_concepts_version <- synapser::synGet('syn51277458')$versionLabel
+  newest_concept_map_version <- synapser::synGet('syn51218416')$versionLabel
   
   pretest_output_concepts <- 
     synapser::synGet('syn51277458', version = 29) %>% 
     {read.csv(.$path)} %>% 
-    dplyr::mutate(dplyr::across(.fns = as.character))
+    dplyr::mutate(dplyr::across(.cols = dplyr::everything(), .fns = as.character))
+  
   pretest_concept_map <- 
     synapser::synGet('syn51218416', version = 29) %>% 
     {read.csv(.$path)}
   
   posttest_output_concepts <- 
-    synapser::synGet('syn51277458', version = 30) %>% 
+    synapser::synGet('syn51277458', version = newest_output_concepts_version) %>% 
     {read.csv(.$path)} %>% 
-    dplyr::mutate(dplyr::across(.fns = as.character))
+    dplyr::mutate(dplyr::across(.cols = dplyr::everything(), .fns = as.character))
+  
   posttest_concept_map <- 
-    synapser::synGet('syn51218416', version = 30) %>% 
+    synapser::synGet('syn51218416', version = newest_concept_map_version) %>% 
     {read.csv(.$path)}
   
   expect_true(all.equal(pretest_output_concepts, posttest_output_concepts))
