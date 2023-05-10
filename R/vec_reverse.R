fb1ea4885bad6d973d296b57a78fabebb8b20d32
pranavanba <pranavanba@gmail.com>
2023-05-10 13:43:22 -0700

    Renamed reverse_str_pairs to vec_reverse and updated title and description

--- R/vec_reverse.R
+++ R/vec_reverse.R
@@ -1,6 +1,6 @@
-#' Reverse order of a named vector
+#' Reverse the order and pairings of a named vector
 #'
-#' `reverse_str_pairs()` takes a named vector as input and returns a new named vector, where both the order of the input vector and its names and values are reversed.
+#' `vec_reverse()` reverses both the order of a named vector and its name = value pairings.
 #'
 #' @param str_pairs A named vector.
 #'
@@ -12,12 +12,12 @@
 #'                   kiwi = "brown", lemon = "yellow")
 #'
 #' # Reverse the order of the named vector and its values
-#' reversed_fruit_colors <- reverse_str_pairs(fruit_colors)
+#' reversed_fruit_colors <- vec_reverse(fruit_colors)
 #'
 #' # Print the original and reversed named vectors
 #' print(fruit_colors)
 #' print(reversed_fruit_colors)
-reverse_str_pairs <- function(str_pairs) {
+vec_reverse <- function(str_pairs) {
   if (!is.vector(str_pairs)) stop("str_pairs must be a vector")
   if (is.numeric(str_pairs)) stop("str_pairs is numeric and must be of type 'character'")
   
