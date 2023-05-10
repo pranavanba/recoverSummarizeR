913680803b695484873d611db4f665e9179ecad0
pranavanba <pranavanba@gmail.com>
2023-05-10 14:17:53 -0700

    Update title, description, and return sections of syn_file_to_df(), and update function to accept column name input as well as parse multiple file types

--- R/syn_file_to_df.R
+++ R/syn_file_to_df.R
@@ -1,24 +1,35 @@
-#' Fetch Synapse CSV file and store in data frame
+#' Fetch a Synapse file and store in data frame
 #'
-#' `syn_file_to_df()` fetches the concept map (ontology) CSV file from Synapse based on the synID of the stored file,
-#' then reads the file to a data frame.
+#' `syn_file_to_df()` fetches a file from Synapse based on the synID of the stored file, then reads the file to a data
+#' frame, with the option to lowercase a specific column. For i2b2/RECOVER, `syn_file_to_df()` can be used to fetch the
+#' concept map (ontology) file from Synapse then read it into a data frame.
 #'
-#' @param synID A Synapse ID for a CSV file in Synapse.
-#' @returns A data frame containing a representation of the data in the file. The values in the "concept_cd" column are
-#'   converted to lowercase.
+#' @param synID A Synapse ID for a file in Synapse.
+#' @returns A data frame containing a representation of the data in the file. The values in the columns specified by
+#'   `cols` are converted to lowercase. For i2b2/RECOVER, the default has been set to `concept_cd`.
 #' @export
 #' @examples
 #' \dontrun{
 #' df <- syn_file_to_df("syn12345678")
 #' }
 #' 
-syn_file_to_df <- function(synID) {
-  # TODO: update to allow specifying which column is 'concept_cd' and maybe rename function to better align with what the
-  # function does (then update description and title as needed)
+syn_file_to_df <- function(synID, cols = "concept_cd") {
   df <- 
     synapser::synGet(synID) %>% 
-    {utils::read.csv(.$path)} %>% 
-    dplyr::mutate(concept_cd = tolower(concept_cd))
+    {
+      if (grepl(".parquet$", .$path)) {
+        arrow::read_parquet(.$path)
+      } else if (grepl(".tsv$", .$path)) {
+        readr::read_tsv(.$path, show_col_types = F)
+      } else if (grepl(".ndjson$", .$path)) {
+        ndjson::stream_in(.$path, cls = "tbl")
+      } else if (grepl(".csv$", .$path)) {
+        utils::read.csv(.$path)
+      } else {
+        stop(paste("Unsupported file format for", .$path))
+      }
+    } %>% 
+    dplyr::mutate(dplyr::across(dplyr::all_of(cols), tolower))
   
   return(df)
 }
