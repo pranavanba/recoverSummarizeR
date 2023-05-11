2e94d858fc5bb5d5787ee23b20fbe3e238040ddb
pranavanba <pranavanba@gmail.com>
2023-05-11 15:27:12 -0700

    Test if summarize_pipeline works for a new synapse outpur folder

--- tests/testthat/test-summarize_pipeline.R
+++ tests/testthat/test-summarize_pipeline.R
@@ -44,3 +44,47 @@ test_that("function works correctly", {
   expect_true(all.equal(pretest_output_concepts, posttest_output_concepts))
   expect_true(all.equal(pretest_concept_map, posttest_concept_map))
 })
+
+# test_that("storing in new syn folder works", {
+#   synapser::synLogin()
+#   
+#   ontologyFileID <- "syn51320791"
+#   parquetDirID <- "syn50996868"
+#   dataset_name_filter <- "fitbit"
+#   concept_replacements <- c("mins" = "minutes",
+#                             "avghr" = "averageheartrate",
+#                             "spo2" = "spo2_",
+#                             "hrv" = "hrv_dailyrmssd",
+#                             "restinghr" = "restingheartrate",
+#                             "sleepbrth" = "sleepsummarybreath")
+#   concept_filter_col <- "concept_cd"
+#   synFolderID <- "syn51475411"
+#   
+#   summarize_pipeline(ontologyFileID = ontologyFileID,
+#                      parquetDirID = parquetDirID,
+#                      dataset_name_filter = dataset_name_filter,
+#                      concept_replacements = concept_replacements,
+#                      concept_filter_col = concept_filter_col,
+#                      synFolderID = synFolderID)
+#   
+#   pretest_output_concepts <- 
+#     synapser::synGet('syn51277458', version = 29) %>% 
+#     {read.csv(.$path)} %>% 
+#     dplyr::mutate(dplyr::across(.cols = dplyr::everything(), .fns = as.character))
+#   
+#   pretest_concept_map <- 
+#     synapser::synGet('syn51218416', version = 29) %>% 
+#     {read.csv(.$path)}
+#   
+#   posttest_output_concepts <- 
+#     synapser::synGet('syn51475534') %>% 
+#     {read.csv(.$path)} %>% 
+#     dplyr::mutate(dplyr::across(.cols = dplyr::everything(), .fns = as.character))
+#   
+#   posttest_concept_map <- 
+#     synapser::synGet('syn51475535') %>% 
+#     {read.csv(.$path)}
+#   
+#   expect_true(all.equal(pretest_output_concepts, posttest_output_concepts))
+#   expect_true(all.equal(pretest_concept_map, posttest_concept_map))
+# })
