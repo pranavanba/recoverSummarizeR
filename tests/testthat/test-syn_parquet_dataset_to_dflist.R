2db47d630fc6185a1dc58726620d691f9c558875
Pranav Anbarasu <pranavanba@gmail.com>
2023-09-25 22:54:29 +0000

    Add tests

--- tests/testthat/test-syn_parquet_dataset_to_dflist.R
+++ tests/testthat/test-syn_parquet_dataset_to_dflist.R
@@ -1,3 +1,26 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("function works", {
+  synDirIDsyn <- "syn50996868"
+  synDirIDsts <- "syn52504331"
+  s3bucket <- "sc-237179673806-pp-rrscq25af2qlg-s3bucket-15qsg0yihrmlf"
+  s3basekey <- "main/parquet"
+  downloadLocation <- "./parquet"
+  dataset_name_filter <- "fitbit"
+  
+  expect_no_error(
+    df_list_syn <- 
+      syn_parquet_dataset_to_dflist(synDirIDsyn, 
+                                    method = "synapse", 
+                                    s3bucket, 
+                                    s3basekey, 
+                                    downloadLocation,
+                                    dataset_name_filter))
+  
+  expect_no_error(
+    df_list_sts <- 
+      syn_parquet_dataset_to_dflist(synDirIDsts, 
+                                    method = "sts", 
+                                    s3bucket, 
+                                    s3basekey, 
+                                    downloadLocation,
+                                    dataset_name_filter))
 })
